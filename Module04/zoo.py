c4df0c35904f548f6e388a1b8997b0c6a9532991
gabney <gabney@ivytech.edu>
2026-02-09 16:46:13 -0500

    update m04 assignment
    finished section 11 questions

---
+++ Module04/zoo.py
@@ -0,0 +1,9 @@
+"""
+This files exists as a module to be called in the m04 programming assignment for step 11.1 and 11.2
+"""
+
+def hours():
+    """
+    This function prints an hours statement to console.
+    """
+    print("Open 9-5 daily.")
